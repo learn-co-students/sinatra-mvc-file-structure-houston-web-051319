4083c7f5e409cd185faaaa92f9b1abe82d430fef
Maruf <marufbinawal@gmail.com>
2019-06-03 16:42:03 -0500

    Done.

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,3 +1,21 @@
-class Model
+class Dog
+    attr_reader :name, :breed, :age
 
+    @@all = []
+
+  def self.all
+    @@all
+  end
+    
+    def initialze (name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+        @all << self
+    end
+
+    def add_dog()
+        Dog.create()
+    end
+        
 end
