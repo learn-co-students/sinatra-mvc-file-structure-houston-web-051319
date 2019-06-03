9044e12b306eacdfe43caf16b3aa0c2442f68f99
feven tsegay <feventsegay6@gmail.com>
2019-06-03 15:52:08 -0500

    Done.

---
+++ app/models/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+    @@dog =[]
+    attr_accessor :name,:breed,:age
+ def initialize(name,breed,age)
+    @name=name
+    @breed=breed
+    @age=age
+    @@dog << self
+ end
+
+ def self.all
+  @@dog
+ end
+
+
+end
